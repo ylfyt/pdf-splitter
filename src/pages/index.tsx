a04417a00cc5e068101d1ca5622edb8d3aa23186
ylfyt <ayudi3521@gmail.com>
2023-02-11 01:22:17 +0700

    Styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export const Home = () => {
 	const [doc, setDoc] = useState<PDFDocument>();
 	const [loading, setLoading] = useState(false);
 	const [splitter, setSplitter] = useState<string[]>(['']);
+	const [message, setMessage] = useState('');
 
 	useEffect(() => {
 		if (!file) {
@@ -71,59 +72,71 @@ export const Home = () => {
 	};
 
 	return (
-		<div className="mt-10 border rounded-lg py-4 px-6">
-			<h1 className="text-3xl text-center mb-4">PDF Splitter</h1>
+		<div className="mt-10 border rounded-lg py-4 px-6 flex flex-col gap-5">
+			<h1 className="text-3xl text-center">PDF Splitter</h1>
 			<div className="">
-				<label className="block mb-2 font-medium text-gray-900">Upload file</label>
-				<div className="flex justify-between">
-					<input
-						accept=".pdf"
-						className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-						id="file_input"
-						type="file"
-						onChange={handleFileChange}
-					/>
+				<label className="text-lg font-medium text-gray-900 mb-4">Upload file</label>
+				<div className="flex justify-between items-center">
+					<input accept=".pdf" className="bg-white file:rounded-md file:bg-gray-300 hover:file:bg-gray-400 w-full p-2 rounded-md" type="file" onChange={handleFileChange} />
 					<div className="w-[150px] font-semibold text-right">{numOfPages ? `${numOfPages} pages` : ''}</div>
 				</div>
 			</div>
 			<div>
-				{splitter.map((val, idx) => {
-					return (
-						<div key={idx}>
-							<input
-								onChange={(e) => {
-									splitter[idx] = e.target.value;
-									setSplitter([...splitter]);
-								}}
-								type="text"
-								value={val}
-							/>
-							{idx === 0 ? (
-								<></>
-							) : (
-								<button
-									onClick={() => {
-										setSplitter(splitter.filter((_, i) => i !== idx));
-									}}
-								>
-									X
-								</button>
-							)}
-						</div>
-					);
-				})}
+				<span className="block text-lg font-medium text-gray-900 mb-1">Split Pages</span>
+				<div className="flex flex-col gap-3">
+					{splitter.map((val, idx) => {
+						return (
+							<div key={idx}>
+								<div className="flex gap-4 items-center">
+									<div>{`Split #${idx + 1}`}</div>
+									<input
+										className="min-w-[400px] text-lg rounded-md px-2 py-1"
+										onChange={(e) => {
+											splitter[idx] = e.target.value;
+											setSplitter([...splitter]);
+										}}
+										type="text"
+										value={val}
+									/>
+									{idx === 0 ? (
+										<></>
+									) : (
+										<button
+											className="font-bold bg-red-300 w-[30px] h-[30px] rounded-full"
+											onClick={() => {
+												setSplitter(splitter.filter((_, i) => i !== idx));
+											}}
+										>
+											&#10005;
+										</button>
+									)}
+								</div>
+							</div>
+						);
+					})}
+				</div>
 				<button
 					onClick={() => {
 						splitter.push('');
 						setSplitter([...splitter]);
 					}}
+					type="button"
+					className="text-white bg-orange-400 focus:outline-none focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-3 py-1 text-center mt-2 shadow-sm"
 				>
-					+
+					Add Split
 				</button>
 			</div>
-			<div>
-				<button onClick={split}>Split</button>
+			<div className="mt-10">
+				<button
+					type="button"
+					disabled={!doc || !file || !splitter.find((val) => val !== '')}
+					onClick={split}
+					className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 shadow-md disabled:opacity-50"
+				>
+					Download
+				</button>
 			</div>
+			<div>{message}</div>
 		</div>
 	);
 };
