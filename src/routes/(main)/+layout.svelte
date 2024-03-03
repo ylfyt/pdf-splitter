54643d1519a31ea1f154d510ac1306d5b7faa052
ylfyt <yalfayat@gmail.com>
2024-03-03 15:17:37 +0700

    Adding service worker for offline support

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -2,14 +2,33 @@
     import '@/index.css';
     import { browser } from '$app/environment';
     import { isDarkStore } from '@/stores/theme';
+    import { onMount } from 'svelte';
 
-    $: {
-        if (browser) {
-            $isDarkStore
-                ? document.documentElement.classList.add('dark')
-                : document.documentElement.classList.remove('dark');
-        }
+    $: if (browser) {
+        $isDarkStore
+            ? document.documentElement.classList.add('dark')
+            : document.documentElement.classList.remove('dark');
     }
+
+    const detectSWUpdate = async () => {
+        const registration = await navigator.serviceWorker.ready;
+
+        registration.addEventListener('updatefound', () => {
+            const newSW = registration.installing;
+            newSW?.addEventListener('statechange', () => {
+                if (newSW.state === 'installed') {
+                    if (confirm('New update available! Reload to update?')) {
+                        newSW.postMessage({ type: 'SKIP_WAITING' });
+                        window.location.reload();
+                    }
+                }
+            });
+        });
+    };
+
+    onMount(() => {
+        detectSWUpdate();
+    });
 </script>
 
 <svelte:head>
---
+++ src/service-worker.ts
@@ -0,0 +1,71 @@
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
+
+import { build, files, version } from '$service-worker';
+
+declare let self: ServiceWorkerGlobalScope;
+
+const CACHE_NAME = `cache-${version}`;
+const ASSETS = [
+    ...build,
+    ...files.filter(el => !el.includes(".nojekyll"))
+];
+
+self.addEventListener('install', e => {
+    const addFileToCache = async () => {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll(ASSETS);
+    };
+    e.waitUntil(addFileToCache());
+});
+
+self.addEventListener('activate', e => {
+    const deleteOldCache = async () => {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE_NAME) {
+                await caches.delete(key);
+            }
+        }
+    };
+    e.waitUntil(deleteOldCache());
+});
+
+self.addEventListener('fetch', e => {
+    if (e.request.method !== 'GET') return;
+
+    const getResponse = async () => {
+        const url = new URL(e.request.url);
+
+        const cache = await caches.open(CACHE_NAME);
+
+        if (ASSETS.includes(url.pathname)) {
+            const cachedResponse = await cache.match(url.pathname);
+            if (cachedResponse) return cachedResponse;
+        }
+
+        try {
+            const response = await fetch(e.request);
+            const isNotExtension = url.protocol === 'http:';
+            const isSuccess = response.status === 200;
+            if (isNotExtension && isSuccess) {
+                cache.put(e.request, response.clone());
+            }
+            return response;
+        } catch {
+            const cachedResponse = await cache.match(url.pathname);
+            if (cachedResponse) return cachedResponse;
+        }
+
+        return new Response('Not found', { status: 404 });
+    };
+
+    e.respondWith(getResponse());
+});
+
+self.addEventListener('message', e => {
+    if (e.data && e.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
+});
